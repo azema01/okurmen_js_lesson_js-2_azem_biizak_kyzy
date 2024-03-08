594413d085951fcc56d5091a9ec0758aa7162687
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-08 17:42:10 +0600

    js 2

---
+++ index.js
@@ -0,0 +1,39 @@
+// let total = 2 + 3;
+// alert('total 2 + 3=' + total);
+// total = 2 + 3 * 2 / 10;
+// alert ('total 2+3 *  2 / 10='+ total);
+// const firstnamber =5;
+// const secondnamber =2;
+// let  $number;
+// let _number;
+// let number2;
+
+
+// const total = first / second;
+
+// alert('total:' +total);
+
+
+const name = 'AZEM';
+const position =  'web dev';
+const stringconcat = name + position;
+
+// //continuation
+// alert('name '+ name + ',' + 'position' + position);
+
+// 
+
+// alert('i get the "a" letter ' + name
+// [0]);
+
+// const stringSlice = name .slice(1, 3);
+// alert('sapty chon tamgadan jazabyz'+ name .toUpperCase() );
+
+// alert('sapty kichine tamga menen jazam '
+// + name.toLowerCase());
+const string = "eI, azem, KaNdAi";
+alert(string[0].toUpperCase() + string [1].toLowerCase()+','+ string[4].
+toUpperCase() + string.slice(5).
+toLowerCase()+ '?');
+
+
